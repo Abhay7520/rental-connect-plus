d79f61b19382bf78d4a25e7a454f73ef8f79a364
Abhay7520 <abhayy7520@gmail.com>
2025-11-23 20:34:43 +0530

    your message

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,23 +1,28 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAln3be8mEk560HOTTkIIC_S1uuuFjPLUw",
-  authDomain: "rental-connect-plus.firebaseapp.com",
-  projectId: "rental-connect-plus",
-  storageBucket: "rental-connect-plus.firebasestorage.app",
-  messagingSenderId: "363141059363",
-  appId: "1:363141059363:web:903411e04904ea6c957296",
-  measurementId: "G-FGS87GJHBX"
+  apiKey: "AIzaSyBp4MRM8BjdKAVxm1EtMndv5vwKa-vbrGs",
+  authDomain: "rental-connect-plus-cf8a5.firebaseapp.com",
+  databaseURL: "https://rental-connect-plus-cf8a5-default-rtdb.firebaseio.com",
+  projectId: "rental-connect-plus-cf8a5",
+  storageBucket: "rental-connect-plus-cf8a5.firebasestorage.app",
+  messagingSenderId: "550359333452",
+  appId: "1:550359333452:web:b57e28e32cd671c2fd52e9",
+  measurementId: "G-PHTKDXCW84"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Initialize Firebase services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
 
 export default app;
